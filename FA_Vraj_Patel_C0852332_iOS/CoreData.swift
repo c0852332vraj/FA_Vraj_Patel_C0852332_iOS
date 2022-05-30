3c14864e63c4ea09b9fc1e7a4ec1ab2198de2380
vraj patel <105397402+c0852332vraj@users.noreply.github.com>
2022-05-30 14:00:12 -0400

    creation Of ui and files

---
+++ FA_Vraj_Patel_C0852332_iOS/CoreData.swift
@@ -0,0 +1,14 @@
+//
+//  CoreData.swift
+//  FA_Vraj_Patel_C0852332_iOS
+//
+//  Created by Vraj Patel on 30/05/22.
+//
+
+import Foundation
+import Foundation
+import CoreData
+
+class CoreDataHelp {
+    
+}
---
+++ FA_Vraj_Patel_C0852332_iOS/MainController.swift
@@ -0,0 +1,34 @@
+//
+//  MainController.swift
+//  FA_Vraj_Patel_C0852332_iOS
+//
+//  Created by Vraj Patel on 30/05/22.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+    
+   
+    @IBOutlet weak var a1: UIButton!
+    @IBOutlet weak var a2: UIButton!
+    @IBOutlet weak var a3: UIButton!
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    @IBOutlet weak var c1: UIButton!
+    @IBOutlet weak var c2: UIButton!
+    @IBOutlet weak var c3: UIButton!
+    @IBOutlet weak var WinX: UILabel!
+    @IBOutlet weak var WinO: UILabel!
+    @IBOutlet weak var turnLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+
+}
+    
+}
